677e447803117c2a85ef97a962ab508d195f282e
enridaga <enricodaga@gmail.com>
2018-05-11 15:34:00 +0100

    Evaluation datasets

--- data/generate_evaluation_data.sh
+++ data/generate_evaluation_data.sh
@@ -7,4 +7,8 @@ echo 'Experiences in the benchmark: in-benchmark.csv'
 cat benchmark/data/*|grep http | sed -e 's/.$//g'|sed -e 's/"//g'|cut -d'/' -f6|sort -n >evaluation/in-benchmark.csv
 echo 'Experiences not in the benchmark: for-training.csv'
 comm -23 evaluation/experiences.csv evaluation/in-benchmark.csv > evaluation/for-training.csv
+echo 'Sources: sources.csv'
+ls benchmark/sources>evaluation/sources.csv
+echo 'Benchmark: benchmark.csv'
+cat benchmark/generated-benchmark.csv |grep -i ".txt,1"|cut -d',' -f1-5>evaluation/benchmark.csv
 
