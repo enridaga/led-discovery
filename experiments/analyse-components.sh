a97375e47f49ab4816b8bf7314406e243623c49b
enridaga <enricodaga@gmail.com>
2019-04-13 15:26:16 +0200

    Updated recent changes

--- experiments/analyse-components.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mvn exec:exec@analyse-components-coverage -Dinput=../data/experiences/ -Doutput=../data/analysis/components-$1-experiences.csv -Dlimit=$1
-mvn exec:exec@analyse-components-coverage -Dinput=../data/negatives-reuters/ -Doutput=../data/analysis/components-$1-reuters.csv -Dlimit=$1
-mvn exec:exec@analyse-components-coverage -Dinput=../data/negatives-red/ -Doutput=../data/analysis/components-$1-red.csv -Dlimit=$1
